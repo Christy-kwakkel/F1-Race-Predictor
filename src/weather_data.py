57ddd55715b1f1487f9549cce7833db6ac1a651b
Christy-kwakkel <christy.kwakkel@student.nhlstenden.com>
2025-10-26 12:30:05 +0100

    simplified the get_weather_data

--- src/weather_data.py
+++ src/weather_data.py
@@ -11,30 +11,27 @@ def get_weather_data(years):
         try: 
             schedule = fastf1.get_event_schedule(year)
             for _, event in schedule.iterrows():
-                try:
-                    round_number = event["RoundNumber"]
-                    session = fastf1.get_session(year, event["RoundNumber"], "R")
-                    session.load(laps=False, telemetry=False, weather=True)
+                session = fastf1.get_session(year, event["RoundNumber"], "R")
+                session.load(laps=False, telemetry=False, weather=True)
+                w = session.weather_data.mean(numeric_onlu=True)
 
-                    if session.weather_data is None or session.weather_data.empty:
-                        print(f"no weather data for {year} round {round_number}")
-                        continue
+                if session.weather_data is None or session.weather_data.empty:
+                    print(f"no weather data for {year}")
+                    continue
 
-                    weather = session.weather_data.mean(numeric_only=True)
-                    rows.append({
-                        "year" : year,
-                        "round": event["RoundNumber"],
-                        "temp_air": weather['AirTemp'],
-                        "temp_track": weather['TrackTemp'],
-                        "humidity": weather['Humidity'],
-                        "wind_speed": weather['WindSpeed'],
-                        "pressure": weather['Pressure'],
-                    })
-                except Exception as e:
-                    print(f" No weather for {year}: {e}")
+                weather = session.weather_data.mean(numeric_only=True)
+                rows.append({
+                    "year" : year,
+                    "round": event["RoundNumber"],
+                    "temp_air": weather['AirTemp'],
+                    "temp_track": weather['TrackTemp'],
+                    "humidity": weather['Humidity'],
+                    "wind_speed": weather['WindSpeed'],
+                    "pressure": weather['Pressure'],
+                })
+
         except Exception as e:
-            print(f"could not load schedule for {year}: {e}")
+            print(f" No weather for {year}: {e}")
     
-    df_weather = pd.DataFrame(rows)
-    print(f"Collected weather data for {len(df_weather)} races")
+    print(f"Collected weather data for races")
     return pd.DataFrame(rows)
